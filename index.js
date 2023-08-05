66209b8c1205674aa3b717b7f4ec7866c9ec76b6
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2023-08-05 20:41:16 +0200

    add js code for input value default to change whenever you click or get user input

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
-/* TODO poprawić input JS */
+// input default value show and hide
+const inputValueDelete = (element) => {
+  element.value = "";
+};
+
+const inputShowHide = () => {
+  const inputList = document.querySelectorAll(".input");
+  for (const element of inputList) {
+    element.addEventListener("click", function () {
+      inputValueDelete(element);
+    });
+  }
+
+  const incomeInput = document.querySelector("#income-name-input");
+  const expanseInput = document.querySelector("#expense-name-input");
+  const amountIncomeInput = document.querySelectorAll("#amount-income-input");
+  const amountExpenseInput = document.querySelectorAll("#amount-expense-input");
+
+  document.body.addEventListener("click", function (event) {
+    const inputId = event.target.getAttribute("id");
+    if (inputId !== "income-name-input") {
+      if (incomeInput.value === "") {
+        incomeInput.value = "Nazwa przychodu";
+      }
+    }
+    if (inputId !== "expense-name-input") {
+      if (expanseInput.value === "") {
+        expanseInput.value = "Nazwa wydatku";
+      }
+    }
+    if (inputId !== "amount-income-input") {
+      for (const input of amountIncomeInput) {
+        if (input.value === "") {
+          input.value = "Kwota";
+        }
+      }
+    }
+    if (inputId !== "amount-expense-input") {
+      for (const input of amountExpenseInput) {
+        if (input.value === "") {
+          input.value = "Kwota";
+        }
+      }
+    }
+  });
+
+  const incomeAddButton = document.querySelector("#income-add-button");
+  incomeAddButton.addEventListener("click", function () {
+    incomeInput.value = "Nazwa przychodu";
+    for (const input of amountIncomeInput) {
+      input.value = "Kwota";
+    }
+  });
+
+  const expanseAddButton = document.querySelector("#expense-add-button");
+  expanseAddButton.addEventListener("click", function () {
+    expanseInput.value = "Nazwa wydatku";
+    for (const input of amountExpenseInput) {
+      input.value = "Kwota";
+    }
+  });
+};
+
+// create list with inputs and add icome and expanse inputs to list
+
+const createIncomeList = () => {
+  const incomeList = document.querySelector("#income-list");
+  incomeList = [];
+
+  //muszę mieć tablicę obiektów -coś nie tak z tablicą
+
+  // muszę złapać input.target.value przychodu + kwoty i t
+  // dodawać do tablicy i dodawac kazdy stworzony element
+  const incomeItem = document.createElement("li");
+  incomeItem.classList.add("icome-item");
+  incomeItem.id = "icome-item";
+  incomeItem.innerText = "przychód1";
+  incomeList.appendChild(incomeItem);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  inputShowHide();
+  createIncomeList();
+});
