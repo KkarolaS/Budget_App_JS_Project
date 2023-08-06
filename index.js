2eb038fe077873da5dab201c29b955fb17b2f3f4
KkarolaS <133031923+KkarolaS@users.noreply.github.com>
2023-08-06 12:42:10 +0200

    change html code, add placeholder to input, change js code and create list of input elements, add delete button and working on change button function

--- index.js
+++ index.js
@@ -1,84 +1,171 @@
-// input default value show and hide
-const inputValueDelete = (element) => {
-  element.value = "";
-};
-
-const inputShowHide = () => {
-  const inputList = document.querySelectorAll(".input");
-  for (const element of inputList) {
-    element.addEventListener("click", function () {
-      inputValueDelete(element);
-    });
-  }
-
-  const incomeInput = document.querySelector("#income-name-input");
-  const expanseInput = document.querySelector("#expense-name-input");
-  const amountIncomeInput = document.querySelectorAll("#amount-income-input");
-  const amountExpenseInput = document.querySelectorAll("#amount-expense-input");
-
-  document.body.addEventListener("click", function (event) {
-    const inputId = event.target.getAttribute("id");
-    if (inputId !== "income-name-input") {
-      if (incomeInput.value === "") {
-        incomeInput.value = "Nazwa przychodu";
-      }
-    }
-    if (inputId !== "expense-name-input") {
-      if (expanseInput.value === "") {
-        expanseInput.value = "Nazwa wydatku";
-      }
-    }
-    if (inputId !== "amount-income-input") {
-      for (const input of amountIncomeInput) {
-        if (input.value === "") {
-          input.value = "Kwota";
-        }
-      }
-    }
-    if (inputId !== "amount-expense-input") {
-      for (const input of amountExpenseInput) {
-        if (input.value === "") {
-          input.value = "Kwota";
-        }
-      }
-    }
-  });
-
-  const incomeAddButton = document.querySelector("#income-add-button");
-  incomeAddButton.addEventListener("click", function () {
-    incomeInput.value = "Nazwa przychodu";
-    for (const input of amountIncomeInput) {
-      input.value = "Kwota";
-    }
-  });
-
-  const expanseAddButton = document.querySelector("#expense-add-button");
-  expanseAddButton.addEventListener("click", function () {
-    expanseInput.value = "Nazwa wydatku";
-    for (const input of amountExpenseInput) {
-      input.value = "Kwota";
-    }
-  });
-};
+const incomeInput = document.querySelector("#income-name-input");
+const expanseInput = document.querySelector("#expense-name-input");
+const amountIncomeInput = document.querySelector("#amount-income-input");
+const amountExpenseInput = document.querySelector("#amount-expense-input");
+const incomeAddButton = document.querySelector("#income-add-button");
+const expanseAddButton = document.querySelector("#expense-add-button");
 
 // create list with inputs and add icome and expanse inputs to list
-
-const createIncomeList = () => {
-  const incomeList = document.querySelector("#income-list");
-  incomeList = [];
-
-  //muszę mieć tablicę obiektów -coś nie tak z tablicą
-
-  // muszę złapać input.target.value przychodu + kwoty i t
-  // dodawać do tablicy i dodawac kazdy stworzony element
+const createInputElement = (value) => {
   const incomeItem = document.createElement("li");
   incomeItem.classList.add("icome-item");
   incomeItem.id = "icome-item";
-  incomeItem.innerText = "przychód1";
-  incomeList.appendChild(incomeItem);
+  incomeItem.textContent = value;
+  return incomeItem;
+};
+
+const createChangeBtn = () => {
+  const changeBtn = document.createElement("button");
+  changeBtn.classList.add("change-btn", "add-btn");
+  changeBtn.id = "change-btn";
+  changeBtn.textContent = "Zmień";
+  return changeBtn;
+};
+
+const createDeleteBtn = () => {
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn", "add-btn");
+  deleteBtn.id = "delete-btn";
+  deleteBtn.textContent = "Usuń";
+  return deleteBtn;
+};
+
+const createTextBtnContainer = () => {
+  const inputBtnContainer = document.createElement("div");
+  inputBtnContainer.id = "input-btn-container";
+  inputBtnContainer.classList.add("input-btn-container");
+  return inputBtnContainer;
+};
+
+const createBtnWrapper = () => {
+  const btnWrapper = document.createElement("div");
+  btnWrapper.id = "btn-wrapper";
+  btnWrapper.classList.add("btn-wrapper");
+  return btnWrapper;
+};
+
+const createIncomeList = () => {
+  const incomeList = document.querySelector("#income-list");
+
+  incomeAddButton.addEventListener("click", function () {
+    const incomeInputName = incomeInput.value;
+    const incomeAmountName = amountIncomeInput.value;
+    if (incomeInputName !== "" && incomeAmountName !== "") {
+      const fullItemName = `${incomeInputName} - ${incomeAmountName} zł`;
+      const elementContainer = createTextBtnContainer();
+      incomeList.appendChild(elementContainer);
+
+      const li = createInputElement(fullItemName);
+      elementContainer.appendChild(li);
+
+      const btnWrapper = createBtnWrapper();
+      elementContainer.appendChild(btnWrapper);
+
+      const changeBtn = createChangeBtn();
+      btnWrapper.appendChild(changeBtn);
+      changeBtn.addEventListener("click", function () {
+        const changeInput = document.createElement("input");
+        changeInput.id = "changeInput";
+        changeInput.value = li.textContent;
+        li.classList.add("hidden");
+        elementContainer.appendChild(changeInput);
+        changeBtn.addEventListener("click", function () {
+          li.textContent = changeInput.value;
+          li.classList.remove("hidden");
+          elementContainer.removeChild(changeInput);
+          //na tym samym buttonie
+          //dodać osobny button na ten click
+        });
+      });
+
+      const deleteBtn = createDeleteBtn();
+      btnWrapper.appendChild(deleteBtn);
+      deleteBtn.addEventListener("click", function () {
+        incomeList.removeChild(elementContainer);
+      });
+
+      incomeInput.value = "";
+      amountIncomeInput.value = "";
+    }
+  });
 };
 
 document.addEventListener("DOMContentLoaded", function () {
-  inputShowHide();
   createIncomeList();
 });
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+// niepotrzebny kod z wartościami value
+
+// const inputValueDelete = (element) => {
+//   element.value = "";
+// };
+// const inputShowHide = () => {
+//   const inputList = document.querySelectorAll(".input");
+//   for (const element of inputList) {
+//     element.addEventListener("click", function () {
+//       inputValueDelete(element);
+//     });
+//   }
+//   document.body.addEventListener("click", function (event) {
+//     const inputId = event.target.getAttribute("id");
+//     if (inputId !== "income-name-input") {
+//       if (incomeInput.value === "") {
+//         incomeInput.value = "Nazwa przychodu";
+//       }
+//     }
+//     if (inputId !== "expense-name-input") {
+//       if (expanseInput.value === "") {
+//         expanseInput.value = "Nazwa wydatku";
+//       }
+//     }
+//     if (inputId !== "amount-income-input") {
+//       if (amountIncomeInput.value === "") {
+//         amountIncomeInput.value = "Kwota";
+//       }
+//     }
+//     if (inputId !== "amount-expense-input") {
+//       if (amountExpenseInput.value === "") {
+//         amountExpenseInput.value = "Kwota";
+//       }
+//     }
+//   });
+//   // const incomeAddButton = document.querySelector("#income-add-button");
+//   incomeAddButton.addEventListener("click", function () {
+//     incomeInput.value = "Nazwa przychodu";
+//     amountIncomeInput.value = "Kwota";
+//   });
+//   // const expanseAddButton = document.querySelector("#expense-add-button");
+//   expanseAddButton.addEventListener("click", function () {
+//     expanseInput.value = "Nazwa wydatku";
+//     amountExpenseInput.value = "Kwota";
+//   });
+// };
+
+// class Item {
+//   constructor(name, amount = 0) {
+//     this.name = name;
+//     this.amount = amount;
+//   }
+//   get fullItemName() {
+//     return `${this.name} ${this.amount} zł`;
+//   }
+// }
+
+// const createNewItemName = () => {
+//   const incomeItemName = incomeInput.value;
+//   const incomeItemAmount = amountIncomeInput.value;
+//   const incomeItem = new Item(incomeItemName, incomeItemAmount);
+//   const incomeItemFullName = incomeItem.fullItemName;
+//   return incomeItemFullName;
+// };
